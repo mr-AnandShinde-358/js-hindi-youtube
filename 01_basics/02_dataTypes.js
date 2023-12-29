81e05e2841b0a15abe221168c33840705f47d1e8
mr-AnandShinde-358 <97289992+mr-AnandShinde-358@users.noreply.github.com>
2023-12-29 18:31:27 +0000

    data_types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hellow"); // we are using nodejs, not browser
+
+console.log(3
+    +
+    3);  // code readability should be high
+
+    // data type in js
+    let number = "hitesh"
+    let age=18;
+    let isLoggedIn = false;
+    let state;
+
+
+    // number =>2 to power 53
+    // bigint => for biggest numbers
+    // string =>""
+    // boolean =>true/false
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+
+    console.log(typeof undefined);  // undefined
+    console.log(typeof null); // null
