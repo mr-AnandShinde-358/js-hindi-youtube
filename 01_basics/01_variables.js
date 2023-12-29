a8bb6cb76b1888196d881493798de86519505058
mr-AnandShinde-358 <97289992+mr-AnandShinde-358@users.noreply.github.com>
2023-12-29 18:07:50 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId =45434 ;
+let accountEmail="hitesh@google.com"
+var acccountPassword ="1234";
+
+accontcity="Jaipur";
+
+
+
+// accountId=2 // not allowed to assing value a const variable
+ 
+/*
+Prefer not to use var because of issue in block scope and functional scope
+
+*/
+
+accountEmail="anand@gmail.com";
+acccountPassword="3498894";
+accontcity="aurangabad";
+
+console.table([accountId,accountEmail,acccountPassword,accontcity]);
