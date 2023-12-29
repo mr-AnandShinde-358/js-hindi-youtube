8c0553ddad24ec7ab7a0767edbf86aeb1b93e4d6
mr-AnandShinde-358 <97289992+mr-AnandShinde-358@users.noreply.github.com>
2023-12-29 19:22:27 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+let score=undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber =Number(score);
+
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
+
+
+// conversion notes
+
+// NaN=> NOt a number
+
+// conversion to number
+
+// "33" => 33
+// "33abc" => NaN
+// true=> 1; false =>0;
+// null => 0
+// undefiend =>NaN 
+
+
+// conversion to boolean
+
+// let isloggedIn ="ahdj";
+let isloggedIn =1;
+
+
+let booleanIsLoggedIn = Boolean(isloggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+
+// 1=> true; 0=>false
+// ""=>false;
+// "hitesh"=>true
+
+
+let someNumber =33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
