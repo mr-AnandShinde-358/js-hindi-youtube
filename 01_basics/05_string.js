6ed74f7858201de4adf6856b5556a9d8581fdb1a
ANAND SHINDE <97289992+mr-AnandShinde-358@users.noreply.github.com>
2023-12-31 09:19:56 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -7,7 +7,7 @@ const repoCount =50;
 // console.log(`Hellow my name is ${name} and my repo count is ${repoCount}`);
 
 
-const gameName=new String('hitesh-hd');
+const gameName=new String('hitesh-hd-com');
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -28,3 +28,27 @@ console.log(anotherString);
 const newStringOne ="   hitesh   ";
 console.log(newStringOne);
 console.log(newStringOne.trim());
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'));
+console.log(url.includes('hitesh'));
+console.log(url.includes('anand'));
+
+console.log(gameName.split('-'));
+
+// NOtes
+
+// When String is called as a constructor (with new), it creates a String object, which is not a primitive.
+
+// When String is called as a function, it coerces the parameter to a string primitive.
+
+/*
+const a = new String("Hello world"); // a === "Hello world" is false
+const b = String("Hello world"); // b === "Hello world" is true
+a instanceof String; // is true
+b instanceof String; // is false
+typeof a; // "object"
+typeof b; // "string"
+
+*/
