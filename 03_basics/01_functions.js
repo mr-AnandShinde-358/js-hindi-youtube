daaa79826dc77075b5e7532278d4e454e5ffeda1
ANAND SHINDE <97289992+mr-AnandShinde-358@users.noreply.github.com>
2024-01-06 04:22:56 +0000

    function with objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -42,4 +42,64 @@ function LoginUserMeassage(username="sam"){
 }
 
 // console.log(LoginUserMeassage()); //  result is undefined
-console.log(LoginUserMeassage("ajay")); 
+// console.log(LoginUserMeassage("ajay"));
+
+// function with objects 
+
+// object destuction lick react
+/* function handleObject({username,price})
+{
+    console.log(`Username is ${username} and price is ${price}`)
+
+}
+
+// handleObject(user)
+handleObject(
+    {
+        username:'Anand',
+        price:529
+    }
+) */
+
+//Rest parameters
+// The rest parameter syntax allows a function to accept an indefinite number of arguments as an array, providing a way to represent variadic functions in JavaScript.
+
+// function calculateCartPrice(...num1){ // output:  [200,300,400,500,2000]
+function calculateCartPrice(val1,val2,...num1){  // output:  [400,500,2000] and assing value val1=200,val2=300
+    return num1;
+}
+
+// console.log(calculateCartPrice(200,300,400,500,2000));
+
+/* const user = {
+    username:"hitesh",
+    prices:199
+}
+
+function handleObject(anyObjects)
+{
+    console.log(`Username is ${anyObjects.username} and price is ${anyObjects.price}`)
+
+}
+
+// handleObject(user)
+handleObject(
+    {
+        username:'sam',
+        price:399
+    }
+) */
+
+
+const myNewArray =[200,400,100,600]
+
+
+function returnSecondValue(getArray){
+    return getArray[2];
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([700,400,600,500,100]));
+
+
+
