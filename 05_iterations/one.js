db0874a42b8cfc8ba5c3bcb2642bdca8701f37e4
ANAND SHINDE <97289992+mr-AnandShinde-358@users.noreply.github.com>
2024-01-07 12:43:11 +0000

    for loop start

---
+++ 05_iterations/one.js
@@ -0,0 +1,18 @@
+// for loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+}
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loops value: ${i}`);
+    console.log(`Table NO: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+      //  console.log(`Inner loops value: ${j} and inner loop ${i}`);
+      console.log(i + '*' + j + '=' + i*j);
+    }
+}
